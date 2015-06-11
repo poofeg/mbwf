eb069eaaf4a9884f354dd326e2c08db01109a69c
Alexey Vaganov <poofeg@gmail.com>
2015-06-11 19:07:30 +0500

    Add license information

--- app/common.h
+++ app/common.h
@@ -1,8 +1,21 @@
 /*
- * common.h
+ * Copyright 2015 Alexey Vaganov
  *
- *  Author: Alexey Vaganov
- */ 
+ * This file is part of mbwf.
+ *
+ * mbwf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mbwf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mbwf.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef COMMON_H_
 #define COMMON_H_
